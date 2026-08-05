8ae3e266665f582b5a6d1c29301d1ce500a62dc0
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-05 13:03:35 +0900

    test: define bounded DollarN recognition contract

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -16,6 +16,7 @@ const SUITES: Array[String] = [
     "res://tests/unit/test_glyph_definition.gd",
     "res://tests/unit/test_glyph_template_repository.gd",
     "res://tests/unit/test_dollar_one_recognizer.gd",
+    "res://tests/unit/test_dollar_n_recognizer.gd",
 ]
 
 func _init() -> void:
