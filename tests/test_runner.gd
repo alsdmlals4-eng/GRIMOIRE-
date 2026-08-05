54657153e1e6f66d7770572bf2914f0575620964
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-05 12:40:25 +0900

    test: define validated Slice template repository contract

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -14,6 +14,7 @@ const SUITES: Array[String] = [
     "res://tests/integration/test_glyph_resource_lifecycle.gd",
     "res://tests/unit/test_glyph_resource_view_model.gd",
     "res://tests/unit/test_glyph_definition.gd",
+    "res://tests/unit/test_glyph_template_repository.gd",
 ]
 
 func _init() -> void:
