f703e29266f8965a34beded9cb4ebdcd0cc1ed3f
alsdmlals4-eng <alsdmlals4@gmail.com>
2026-08-06 00:04:45 +0900

    fix: reject crossing Stage2 circuit edges

--- src/core/stage2/stage2_circuit_validator.gd
+++ src/core/stage2/stage2_circuit_validator.gd
@@ -63,6 +63,7 @@ func validate(nodes: Array, edges: Array) -> Dictionary:
     var adjacency: Dictionary = {}
     var out_degree: Dictionary = {}
     var seen_edges: Dictionary = {}
+    var segments: Array[Dictionary] = []
     for node_id in by_id.keys():
         adjacency[node_id] = []
         out_degree[node_id] = 0
@@ -88,6 +89,18 @@ func validate(nodes: Array, edges: Array) -> Dictionary:
             return _invalid(&"TARGET_MUST_BE_TERMINAL_LEAF")
         if not _adjacent(from_node.get("cell"), to_node.get("cell")):
             return _invalid(&"EDGE_NOT_ADJACENT")
+
+        var segment := {
+            "from_id": from_id,
+            "to_id": to_id,
+            "from_cell": Vector2(from_node.get("cell")),
+            "to_cell": Vector2(to_node.get("cell")),
+        }
+        for previous in segments:
+            if _segments_cross_without_shared_endpoint(previous, segment):
+                return _invalid(&"CROSSING_EDGES_PROHIBITED")
+        segments.append(segment)
+
         adjacency[from_id].append(to_id)
         out_degree[from_id] = int(out_degree[from_id]) + 1
 
@@ -134,6 +147,29 @@ func _adjacent(a_variant: Variant, b_variant: Variant) -> bool:
     var b: Vector2i = b_variant
     var delta := (a - b).abs()
     return delta.x <= 1 and delta.y <= 1 and delta != Vector2i.ZERO
+
+
+func _segments_cross_without_shared_endpoint(a: Dictionary, b: Dictionary) -> bool:
+    var a_from_id := StringName(a.get("from_id", &""))
+    var a_to_id := StringName(a.get("to_id", &""))
+    var b_from_id := StringName(b.get("from_id", &""))
+    var b_to_id := StringName(b.get("to_id", &""))
+    if a_from_id in [b_from_id, b_to_id] or a_to_id in [b_from_id, b_to_id]:
+        return false
+
+    var a_from: Vector2 = a.get("from_cell")
+    var a_to: Vector2 = a.get("to_cell")
+    var b_from: Vector2 = b.get("from_cell")
+    var b_to: Vector2 = b.get("to_cell")
+    var side_a1 := _orientation(a_from, a_to, b_from)
+    var side_a2 := _orientation(a_from, a_to, b_to)
+    var side_b1 := _orientation(b_from, b_to, a_from)
+    var side_b2 := _orientation(b_from, b_to, a_to)
+    return side_a1 * side_a2 < 0.0 and side_b1 * side_b2 < 0.0
+
+
+func _orientation(origin: Vector2, endpoint: Vector2, point: Vector2) -> float:
+    return (endpoint - origin).cross(point - origin)
 
 
 func _invalid(status: StringName) -> Dictionary:
